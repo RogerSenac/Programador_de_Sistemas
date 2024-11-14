049e9349cfca42577fba31129469411573896146
Rogério Wagner <roger2207valente@gmail.com>
2024-11-14 16:10:21 -0300

    Exercicios resolvidos das listas anteiores

---
+++ Exercicios/Exercícios/JavaScript/lib/Exercicio8.js
@@ -0,0 +1,11 @@
+function calcularquadrado (a) { 
+let total;
+total =  Number (a) * Number (a);
+
+return total;
+}
+
+console.log(calcularquadrado(2));
+console.log(calcularquadrado(5));
+console.log(calcularquadrado(10));
+console.log(calcularquadrado(0.5));
---
+++ Exercicios/Exercícios/JavaScript/lib/Exercicio9.js
@@ -0,0 +1,11 @@
+function resto (x, y) {
+var n1 = Number(x)
+var n2 = Number(y)
+var resultado = n1 % n2 
+return resultado
+} 
+console.log (resto(20, 3))
+console.log (resto(25, 5))
+console.log (resto(100, 10))
+console.log (resto(100, 30))
+console.log (resto(100, 33))
