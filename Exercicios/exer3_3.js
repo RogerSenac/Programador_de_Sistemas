a8c2014d5394646d081c8fd0cfd6de50e3a5c951
RogerSenac <roger2207valente@gmail.com>
2024-12-17 15:20:14 -0300

    Exercicio Verificar Sinal

--- Exercicios/exer3_3.js
+++ Exercicios/exer3_3.js
@@ -1,16 +1,4 @@
-function verificarSinal(numero) {
-    const numeroConvertido = Number(numero);
-  
-    if (isNaN(numeroConvertido)) {
-      return "Entrada inválida: não é um número";
-    } else if (numeroConvertido > 0) {
-      return "Positivo";
-    } else if (numeroConvertido < 0) {
-      return "Negativo";
-    } else {
-      return "Zero";
-    }
-  }
+import {verificarSinal} from "./biblioteca.js";
   
   // Testando a função com os cenários:
   console.log(verificarSinal(5));   // Saída: Positivo
