8ef1d815740ff16edf4ed5848275ea35ba09fc21
RogerSenac <roger2207valente@gmail.com>
2024-12-17 15:18:37 -0300

    Exercicio Verificar Par Impar

--- Exercicios/exer3_2.js
+++ Exercicios/exer3_2.js
@@ -1,14 +1,8 @@
-function verificarParImpar(numero) {
-  //if (numero % 2 === 0) {
-  //  return "Par";
-  // } else {
-  //   return "Ímpar";
-  // }
-return numero % 2===0 ? "Par": "Impar";
-}
-console.log()
+import {verificarParImpar} from "./biblioteca.js"; 
+
+console.log();
 // Testando a função com os cenários:
-//console.log(verificarParImpar(4));   // Saída: Par
-//console.log(verificarParImpar(7));   // Saída: Ímpar
-//console.log(verificarParImpar(0));   // Saída: Par
-//console.log(verificarParImpar(-5));  // Saída: Ímpar
+console.log(verificarParImpar(4));   // Saída: Par
+console.log(verificarParImpar(7));   // Saída: Ímpar
+console.log(verificarParImpar(0));   // Saída: Par
+console.log(verificarParImpar(-5));  // Saída: Ímpar
